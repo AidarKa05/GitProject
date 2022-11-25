5dada0b7fd9a82382ff2841ee95fcd4c0ca89e66
AidarKa05 <118551336+AidarKa05@users.noreply.github.com>
2022-11-25 19:28:06 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,22 @@
-def main(name):
-    print(f'Hi, {name}')
+import pygame
 
 
-if __name__ == '__main__':
-    main('PyCharm')
+pygame.init()
+a, n = [int(el) for el in input().split()]
+if a % n == 0:
+    width = a / n
+    size = (a, a)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Шахматная клетка')
+    screen.fill((0, 0, 0))
+    for x in range(n):
+        for y in range(n):
+            if x % 2 == 0 and y % 2 == 0:
+                screen.fill((255, 255, 255), pygame.Rect(x * width, y * width, x * width + width, y * width + width))
+
+    while pygame.event.wait().type != pygame.QUIT:
+        pygame.display.flip()
+
+    pygame.quit()
+else:
+    print('Неправильный формат ввода')
